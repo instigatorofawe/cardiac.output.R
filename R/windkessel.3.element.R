1b19804458d98ab76ef9a45e12b3d173e8a7c83a
Ran Liu <rliu14@jhu.edu>
2018-06-25 18:18:17 -0400

    Update, slight bugfixes

--- R/windkessel.3.element.R
+++ R/windkessel.3.element.R
@@ -1,5 +1,5 @@
 #' windkessel.three.element
-#' 
+#'
 #' Generates data according to the 3-element windkessel model
 #' @param T end time
 #' @param N number of discrete time steps for the interval [0,T]
@@ -12,19 +12,19 @@
 #' @export
 windkessel.three.element <- function(T, N, r1, r2, c, p0, cardiac.output) {
   times = seq(0, T, by=T/(N-1))
-  
+
   state_evolution = function(t, state, params) {
     with(as.list(c(state)), {
       dx = cardiac.output(t)/c - x/(r2*c)
       return(list(dx))
     })
   }
-  
+
   result = deSolve::ode(y = c(x=p0), times = times, func = state_evolution, parms=NULL, rtol = 1e-10, atol = 1e-10)
-  cardiac_output = i(times)
-  
+  cardiac_output = cardiac.output(times)
+
   result[,2] = result[,2] + cardiac_output * r1
-  
+
   return(result)
-  
+
 }
